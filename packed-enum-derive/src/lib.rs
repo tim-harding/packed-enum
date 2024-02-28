ff673d9218ff924a5a779da324ffea91f8c2e440
Tim Harding <tim@timharding.co>
2024-02-28 15:35:52 -0800

    Push and pop work

--- packed-enum-derive/src/lib.rs
+++ packed-enum-derive/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, TokenStream as TokenStream2};
 use quote::{format_ident, quote, quote_spanned};
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Variant};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Variant};
 
 #[proc_macro_derive(Packed)]
 pub fn packed(input: TokenStream) -> TokenStream {
@@ -30,10 +30,11 @@ fn packed_inner(input: DeriveInput) -> Result<TokenStream2, PackedError> {
     let ident_snake = to_snake_ident(&ident);
     let strukts_mod = format_ident!("{ident_snake}_variants");
     let enum_vec = format_ident!("{ident}Packed");
+    let discriminants = format_ident!("{ident}Discriminants");
 
     match data {
         Data::Enum(e) => {
-            let (fields, (ident_variant_snake, ident_variant)): (Vec<_>, (Vec<_>, Vec<_>)) = e
+            let (fields, ident_variant): (Vec<_>, Vec<_>) = e
                 .variants
                 .into_iter()
                 .map(|variant| {
@@ -43,29 +44,127 @@ fn packed_inner(input: DeriveInput) -> Result<TokenStream2, PackedError> {
                         discriminant: _,
                         attrs: _,
                     } = variant;
-                    (fields, (to_snake_ident(&ident), ident))
+                    (fields, ident)
                 })
                 .unzip();
 
-            let strukts: Vec<_> = fields
+            let ident_variant_snake: Vec<_> = ident_variant
+                .iter()
+                .map(|ident| to_snake_ident(&ident))
+                .collect();
+
+            let strukt_definitions: Vec<_> = fields
                 .iter()
                 .zip(&ident_variant)
                 .map(|(fields, ident_variant)| match fields {
-                    Fields::Named(fields) => quote! { #ident_variant #fields },
-                    Fields::Unnamed(fields) => quote! { #ident_variant #fields; },
+                    Fields::Named(fields) => {
+                        let definition = fields.named.iter().map(|field| {
+                            let Field { ident, ty, .. } = field;
+                            quote! {
+                                pub #ident: #ty,
+                            }
+                        });
+                        quote! {
+                            #ident_variant {
+                                #(#definition)*
+                            }
+                        }
+                    }
+
+                    Fields::Unnamed(fields) => {
+                        let definition = fields.unnamed.iter().map(|field| {
+                            let Field { ty, .. } = field;
+                            quote! {
+                                pub #ty
+                            }
+                        });
+                        quote! {
+                            #ident_variant ( #(#definition),* );
+                        }
+                    }
+
                     Fields::Unit => quote! { #ident_variant; },
+                })
+                .collect();
+
+            let shapes: Vec<_> = fields
+                .iter()
+                .map(|fields| match fields {
+                    Fields::Named(fields) => {
+                        let field_idents = fields.named.iter().map(|field| &field.ident);
+                        quote! {
+                            {
+                                #(#field_idents,)*
+                            }
+                        }
+                    }
+
+                    Fields::Unnamed(fields) => {
+                        let field_idents = fields
+                            .unnamed
+                            .iter()
+                            .enumerate()
+                            .map(|(index, _)| format_ident!("f_{}", index));
+                        quote! {
+                            ( #(#field_idents),* )
+                        }
+                    }
+
+                    Fields::Unit => quote! {},
                 })
                 .collect();
 
             Ok(quote! {
                 #[automatically_derived]
                 mod #strukts_mod {
-                    #(pub struct #strukts)*
+                    #(pub struct #strukt_definitions)*
+                }
+
+                #vis enum #discriminants {
+                    #( #ident_variant, )*
                 }
 
                 #[automatically_derived]
+                #[derive(Default)]
                 #vis struct #enum_vec {
-                    #(pub #ident_variant_snake: Vec<#strukts_mod::#ident_variant>,)*
+                    // Field index, index within field
+                    indices: Vec<(#discriminants, usize)>,
+                    #(
+                        #ident_variant_snake: Vec<#strukts_mod::#ident_variant>,
+                    )*
+                }
+
+                #[automatically_derived]
+                impl #enum_vec {
+                    pub fn new() -> Self {
+                        Self::default()
+                    }
+
+                    pub fn push(&mut self, element: #ident) {
+                        match element {
+                            #(
+                                #ident::#ident_variant #shapes => {
+                                    let strukt = #strukts_mod::#ident_variant #shapes;
+                                    let index = self.#ident_variant_snake.len();
+                                    self.#ident_variant_snake.push(strukt);
+                                    self.indices.push((#discriminants::#ident_variant, index));
+                                }
+                            )*
+                        }
+                    }
+
+                    pub fn pop(&mut self) -> Option<#ident> {
+                        let (field_index, _) = self.indices.pop()?;
+                        match field_index {
+                            #(
+                                #discriminants::#ident_variant => {
+                                    let #strukts_mod::#ident_variant #shapes =
+                                        self.#ident_variant_snake.pop()?;
+                                    Some(#ident::#ident_variant #shapes)
+                                }
+                            )*
+                        }
+                    }
                 }
             })
         }
--- packed-enum-testing/src/lib.rs
+++ packed-enum-testing/src/lib.rs
@@ -1,9 +1,9 @@
 #![cfg(test)]
+#![allow(unused)]
 
 use packed_enum::Packed;
 
-#[allow(unused)]
-#[derive(Packed)]
+#[derive(Packed, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 enum Test {
     Things,
     Stuff(u8),
@@ -11,4 +11,13 @@ enum Test {
 }
 
 #[test]
-fn stuff() {}
+fn stuff() {
+    let mut v = TestPacked::new();
+    v.push(Test::Things);
+    v.push(Test::Stuff(10));
+    v.push(Test::Foo { bar: 20, baz: 30 });
+    assert_eq!(v.pop(), Some(Test::Foo { bar: 20, baz: 30 }));
+    assert_eq!(v.pop(), Some(Test::Stuff(10)));
+    assert_eq!(v.pop(), Some(Test::Things));
+    assert_eq!(v.pop(), None);
+}
